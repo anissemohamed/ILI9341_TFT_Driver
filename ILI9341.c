a8715745259128cac33f6ce6f851cbdc5190c46b
anissemohamed <anisse.mohamed@gmail.com>
2014-07-23 19:50:38 +0200

    Change Hakim familly name

--- ILI9341.c
+++ ILI9341.c
@@ -1,6 +1,6 @@
 /* Library for 240*320 pixel display TFT based on ILI9341 LCD Controller
  *
- * Copyright (c) 2014 Anisse MOHAMED - Hakim ELGARRABI
+ * Copyright (c) 2014 Anisse MOHAMED - Hakim ELGHARRABI
  */
  
  #include "ILI9341.h"
